0de174d7e8d7dfd30e73a30fd4a45e590ebfd537
tndud042713 <tndud042713@gmail.com>
2020-12-22 07:01:58 +0900

    ## js - 단어퍼즐 10개정도의 임의의 단어에서 무작위로 고르는기능 버튼으로 배열된 단어의 위치를 섞어주는 기능 Math.random()을 적재적소에 활용 할 수 있다.

--- JS_WordPuzzle/word.js
+++ JS_WordPuzzle/word.js
@@ -1,13 +1,30 @@
-var str = document.getElementById('word1').innerHTML;
+var word1 = document.getElementById('word1');
 var word2 = document.getElementById('word2');
+var check = document.getElementById('check');
 
-var game = {};
-game.word = str.split('');
+var words = 'apple,linux,javascript,tutorial,baby,girlfriend,legend'.split(','); //이렇게 하면 배열에 저것들을 넣을 수 있음
+var game={};
+game.choice = function(){
+    var idx = Math.floor(Math.random()*words.length);
+    return words[idx];
+}
+var answer = game.choice();
+word1.innerHTML = answer;
+
+game.word = answer.split('');
 game.btns = [];
 
-for (var i = 0; i < str.length; i++) {
+game.updateDisplay = function(){
+    if(answer == game.word.join('')){
+        check.innerHTML = '일치합니다';
+    }else{
+        check.innerHTML = '일치하지 않습니다';
+    }
+}
+
+for (var i = 0; i < answer.length; i++) {
     var btn = document.createElement('button');
-    btn.innerHTML = str[i];
+    btn.innerHTML = answer[i];
     word2.appendChild(btn);
     game.btns.push(btn);
 }
@@ -20,6 +37,14 @@ game.copyBtnText = function () {
 
 var swap = function (event) {
     console.log('swap');
+    var temp = []; //copy and swap
+    while(game.word.length !=0){
+        var s = game.word.pop();
+        temp.push(s);
+    }
+    game.word = temp;
+    game.copyBtnText();
+    game.updateDisplay();
 };
 
 
@@ -37,3 +62,18 @@ var lshift = function (event) {
     game.word.push(s); //오른쪽으로 넣는다.
     game.copyBtnText();
 };
+
+//shuffle
+
+var toggle = Math.floor(Math.random()*2) === 0;
+
+if(toggle){
+    // swap(); -실제로 섞이는지 확인하기전 swap 되는지 확인하는 코드
+}
+
+var n = Math.floor(Math.random() * answer.length);
+
+for(var i=0; i<n ; i++){
+    rshift();
+
+}
