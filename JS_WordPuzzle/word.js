1a5d4f1b40e1c98808055ff8a9c81af208e5b481
tndud042713 <tndud042713@gmail.com>
2020-12-22 15:38:16 +0900

    JS_버튼구현
    객체 중심으로 정리해놓기
    이렇게 해놓으면 나중에 코드 보기도 깔끔하고 실수도 줄일수 있다.

--- JS_WordPuzzle/word.js
+++ JS_WordPuzzle/word.js
@@ -1,79 +1,91 @@
-var word1 = document.getElementById('word1');
-var word2 = document.getElementById('word2');
-var check = document.getElementById('check');
+var word1 = document.getElementById('word1'); // answer 
+var word2 = document.getElementById('word2'); // buttons
+var check = document.getElementById('check'); // word1 === word2? 조건을 위해서 필요함
 
-var words = 'apple,linux,javascript,tutorial,baby,girlfriend,legend'.split(','); //이렇게 하면 배열에 저것들을 넣을 수 있음
-var game={};
-game.choice = function(){
-    var idx = Math.floor(Math.random()*words.length);
-    return words[idx];
-}
-var answer = game.choice();
-word1.innerHTML = answer;
+//game objects
+var game = { 'btns': [] };
+game.words = 'apple,linux,javascript,tutorial,baby,girlfriend,legend'.split(','); //이렇게 하면 배열에 저것들을 넣을 수 있음
 
-game.word = answer.split('');
-game.btns = [];
+//choose 1 word from words;
+game.choose = function () {
+    var idx = Math.floor(Math.random() * this.words.length);
+    this.answer = this.words[idx];
+    this.letters = this.answer.split('');
+    word1.innerHTML = this.answer;
+};
 
-game.updateDisplay = function(){
-    if(answer == game.word.join('')){
-        check.innerHTML = '일치합니다';
-    }else{
-        check.innerHTML = '일치하지 않습니다';
-    }
-}
-
-for (var i = 0; i < answer.length; i++) {
-    var btn = document.createElement('button');
-    btn.innerHTML = answer[i];
-    word2.appendChild(btn);
-    game.btns.push(btn);
-}
-
-game.copyBtnText = function () {
-    for (var i = 0; i < this.word.length; i++) {
-        this.btns[i].innerHTML = this.word[i];
+game.addButtons = function () {
+    for (var i = 0; i < this.letters.length; i++) {
+        var btn = document.createElement('button');
+        btn.innerHTML = this.letters[i];
+        word2.appendChild(btn);
+        this.btns.push(btn);
     }
 };
 
-var swap = function (event) {
-    console.log('swap');
+game.updateDisplay = function () {
+    var gameStr = this.letters.join('');
+    if (this.answer == gameStr) {
+        check.innerHTML = '일치합니다';
+    } else {
+        check.innerHTML = '일치하지 않습니다';
+    }
+};
+
+game.init = function () {
+    this.choose();
+    this.addButtons();
+    this.updateDisplay();
+};
+
+game.init();
+
+game.copyBtnText = function () {
+    for (var i = 0; i < this.letters.length; i++) {
+        this.btns[i].innerHTML = this.letters[i];
+    }
+};
+
+//event handler for swap button
+var swap = function () {
+    // console.log('swap');//확인용 콘솔
     var temp = []; //copy and swap
-    while(game.word.length !=0){
-        var s = game.word.pop();
+    while (game.letters.length != 0) {
+        var s = game.letters.pop();
         temp.push(s);
     }
-    game.word = temp;
+    game.letters = temp;
     game.copyBtnText();
     game.updateDisplay();
 };
 
 
 
-var rshift = function (event) {
-    console.log('rshift');
-    var s = game.word.pop();
-    game.word.unshift(s); // 강사풀이가 훨씬 간결하고 단순함
+var rshift = function () {
+    // console.log('rshift');//확인용 콘솔
+    var s = game.letters.pop();
+    game.letters.unshift(s); // 강사풀이가 훨씬 간결하고 단순함
     game.copyBtnText();
+    game.updateDisplay();
 };
 
-var lshift = function (event) {
-    console.log('lshift');
-    var s = game.word.shift(); //왼쪽에서 빼고
-    game.word.push(s); //오른쪽으로 넣는다.
+var lshift = function () {
+    // console.log('lshift');//확인용 콘솔
+    var s = game.letters.shift(); //왼쪽에서 빼고
+    game.letters.push(s); //오른쪽으로 넣는다.
     game.copyBtnText();
+    game.updateDisplay();
 };
 
 //shuffle
-
-var toggle = Math.floor(Math.random()*2) === 0;
-
-if(toggle){
-    // swap(); -실제로 섞이는지 확인하기전 swap 되는지 확인하는 코드
-}
-
-var n = Math.floor(Math.random() * answer.length);
-
-for(var i=0; i<n ; i++){
-    rshift();
-
-}
+game.shuffle = function () {
+    var toggle = Math.floor(Math.random() * 2) === 0;
+    if (toggle) {
+        swap();
+    }
+    var n = Math.floor(Math.random() * game.answer.length);
+    for (var i = 0; i < n; i++) {
+        rshift();
+    }
+};
+game.shuffle(); // 위에 만들어준 함수 호출
