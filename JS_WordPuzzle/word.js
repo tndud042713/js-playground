b381e30231377037429a0e705c5c1507e40aabad
tndud042713 <tndud042713@gmail.com>
2020-12-22 17:36:40 +0900

    기능구현 한문제를 맞추면 다음문제를 출제한다 한문제를 맞출때마다 상단에 O표시 해준다 세문제를 맞출시에는 Thank you playing을 alert로 표시한다.

--- JS_WordPuzzle/word.js
+++ JS_WordPuzzle/word.js
@@ -1,9 +1,14 @@
 var word1 = document.getElementById('word1'); // answer 
 var word2 = document.getElementById('word2'); // buttons
 var check = document.getElementById('check'); // word1 === word2? 조건을 위해서 필요함
+var progress = document.getElementById('progress'); // progress check
 
 //game objects
-var game = { 'btns': [] };
+var game = {
+    'btns': [],
+    'maxPlay': 3,
+    'current': 0
+};
 game.words = 'apple,linux,javascript,tutorial,baby,girlfriend,legend'.split(','); //이렇게 하면 배열에 저것들을 넣을 수 있음
 
 //choose 1 word from words;
@@ -22,10 +27,20 @@ game.addButtons = function () {
         this.btns.push(btn);
     }
 };
+game.removeButtons = function(){
+    for(var i=0;i<this.btns.length;i++){
+        word2.removeChild(this.btns[i]);
+    }
+    this.btns = [];
+};
+
+game.checkGood = function(){
+    return this.answer === this.letters.join('');
+};
 
 game.updateDisplay = function () {
-    var gameStr = this.letters.join('');
-    if (this.answer == gameStr) {
+    // var gameStr = this.letters.join('');
+    if (this.checkGood()) {
         check.innerHTML = '일치합니다';
     } else {
         check.innerHTML = '일치하지 않습니다';
@@ -46,8 +61,7 @@ game.copyBtnText = function () {
     }
 };
 
-//event handler for swap button
-var swap = function () {
+game.swap = function () {
     // console.log('swap');//확인용 콘솔
     var temp = []; //copy and swap
     while (game.letters.length != 0) {
@@ -58,10 +72,7 @@ var swap = function () {
     game.copyBtnText();
     game.updateDisplay();
 };
-
-
-
-var rshift = function () {
+game.rshift = function () {
     // console.log('rshift');//확인용 콘솔
     var s = game.letters.pop();
     game.letters.unshift(s); // 강사풀이가 훨씬 간결하고 단순함
@@ -69,7 +80,7 @@ var rshift = function () {
     game.updateDisplay();
 };
 
-var lshift = function () {
+game.lshift = function () {
     // console.log('lshift');//확인용 콘솔
     var s = game.letters.shift(); //왼쪽에서 빼고
     game.letters.push(s); //오른쪽으로 넣는다.
@@ -77,15 +88,47 @@ var lshift = function () {
     game.updateDisplay();
 };
 
+game.progress = function(){
+    if (game.checkGood()){
+        game.current++; // 맞출때마다 1 증가시켜줌
+        game.removeButtons();
+        game.init(); // 맞출때마다 게임을 초기화시켜줌
+        game.shuffle(); // 맞출때 다시 섞어야함
+        var str = "";
+        for(var i=0; i<game.current; i++){
+            str += "O";// 화면에 하나씩 O를 추가한다
+        }
+        progress.innerHTML = str; // O를 progress 파트에 추가한다.
+    }
+    if (game.current == game.maxPlay){
+        alert("Good! Thank you for playing");
+    }
+};
+//event handler for swap button
+var swap = function () {
+    game.swap();
+    game.progress();
+};
+
+var rshift = function () {
+    game.rshift();
+    game.progress();
+};
+
+var lshift = function () {
+    game.lshift();
+    game.progress();
+};
+
 //shuffle
 game.shuffle = function () {
-    var toggle = Math.floor(Math.random() * 2) === 0;
+    var toggle = Math.floor(Math.random() * 2) === 0; //toggle 이 0이냐 1이냐    
     if (toggle) {
-        swap();
+        game.swap();
     }
-    var n = Math.floor(Math.random() * game.answer.length);
+    var n = Math.floor(Math.random() * (game.answer.length-1));
     for (var i = 0; i < n; i++) {
-        rshift();
+        game.rshift();
     }
 };
 game.shuffle(); // 위에 만들어준 함수 호출
